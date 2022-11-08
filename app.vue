6a0bf9cfa20b1a6646cd91f69345b977d5abe370
49480575+WinterYukky@users.noreply.github.com <49480575+WinterYukky@users.noreply.github.com>
2022-11-08 19:20:16 +0900

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-app-bar color="blue">
+      <v-toolbar-title>Sample App</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container style="max-width: 600px">
+        <h1>Hello Nuxt3 & Vuetify</h1>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  ssr: false,
+  css: ['vuetify/lib/styles/main.sass'],
+  typescript: {
+    shim: false,
+    strict: true,
+  },
+})
